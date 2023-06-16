a883177e4e5f2e9603c33c0ca2be4768ba76b38b
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-15 19:53:09 -0500

    Creating useRef in Formulario to nombreInput, apellidoInput, emailInput, mi caja, also we create a function named mostrar to capture current.value and also we create a form

---
+++ src/components/Formulario.js
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react'
+
+export const Formulario = () => {
+  
+    const nombreInput = useRef();
+    const apellidoInput = useRef();
+    const emailInput = useRef();
+    const miCaja = useRef();
+
+    const mostrar = e => {
+        e.preventDefault();
+
+        console.log(nombreInput.current.value);
+        console.log(apellidoInput.current.value);
+        console.log(emailInput.current.value);
+
+        //mi caja
+        console.log(miCaja)
+        let { current: caja} = miCaja;
+        caja.classList.add("fondoVerde");
+        caja.innerHTML = "Formulario Enviado!";
+    }
+  
+  return (
+    <div>
+        <h1>Formulario</h1>
+
+
+    <div ref={miCaja} className='miCaja'>
+        <h2>Pruebas con useRef</h2>
+    </div>
+
+    <form onSubmit={mostrar}>
+        <input type='text' placeholder='Nombre' ref={nombreInput} /><br/>
+        <input type='text' placeholder='Apellidos' ref={apellidoInput}/><br/>
+        <input type='email' placeholder='Correo Electrónico' ref={emailInput}/><br/>
+
+        <input type="submit" value="Enviar" />
+    </form>
+
+    <button onClick={() => nombreInput.current.select()}>Empezar a rellenar el formulario</button>
+</div>
+  )
+}
