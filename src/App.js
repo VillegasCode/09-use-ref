de2093011a726ea51ba39c2054ed2bafda931325
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-17 09:10:47 -0500

    Importing Ejemplo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Ejemplo } from "./components/Ejemplo";
 import { Formulario } from "./components/Formulario";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Ejemplo />
 
         <Formulario/>
       </header>
